4addde000921807f22a37136b80c376917f98c16
Darius Nau <109037333+theropod1@users.noreply.github.com>
2025-05-11 00:18:57 +0200

    Update paleodiv_example.R

--- paleodiv_example.R
+++ paleodiv_example.R
@@ -1,13 +1,15 @@
 library(paleoDiv)
 library(strap)
 library(paleotree)
+library(rphylopic)
+source("https://raw.githubusercontent.com/theropod1/R-scripts/refs/heads/main/ppsource.R") #for ppsource function that automatically summarizes the sources for phylopic silhouettes
 #####
 
 #download
 read.tree(text="(Enantiornithes,(Hesperornithiformes,(Palaeognathae,(Galloanserae,Odontopterygiformes,Neoaves(Columbiformes,Passerea((Cuculiformes,Strisores),((Gruiformes,Charadriiformes),(Aequornithes,Telluraves(((Accipitriformes,Strigiformes),(Coraciiformes,Piciformes)),(Cariamiformes,(Falconiformes,(Psittaciformes,Passeriformes))))))))))));")->tree0
-if(!exists("treedata0")) pdb.autodiv(tree0)->treedata0
+if(!exists("treedata0")) pdb.autodiv(tree0)->treedata0 #only start completely new download if dataset is not already downloaded; this saves time if script is executed repeatedly
 
-if(exists("treedata0")){
+if(exists("treedata0")){ 
 treedata1<-list()
 
 tree0$tip.label[!(tree0$tip.label%in%names(treedata0))]->addtax
@@ -44,3 +46,18 @@ abline(v=tsconv(c(315,260,252,201.3,143,89,66,23),tree0),lwd=c(1,2,2,2,1,1,2,1),
 #label clades
 nodelabels(c(1:7),c(34,31,30,25,24,23,21),col="black", bg="white",frame="circle",cex=0.8)
 legend("topleft",pch=cc(1:7), legend=c("Australaves","Afroaves","Telluraves","Passerea","Neoaves","Neognathae","Neornithes"), bg=add.alpha("white",0.8), box.col=NA)
+
+
+##add images from phylopic:
+if(!exists("img")) list()->img
+
+for(i in 1:length(tree0$tip.label)){
+
+if(!exists("img")){img[[tree0$tip.label[i]]] <- get_phylopic(uuid = get_uuid(name = tree0$tip.label[i],n=1)[1])
+}else if(!(tree0$tip.label[i]%in%names(img))){
+img[[tree0$tip.label[i]]] <- get_phylopic(uuid = get_uuid(name = tree0$tip.label[i],n=1)[1])
+}
+
+add_phylopic_base(img = img[[tree0$tip.label[i]]], x = tsconv(60,tree0), y = i, ysize = 1, fill=add.alpha(ggcol(length(tree0$tip.label))[i],00.50))
+}
+
