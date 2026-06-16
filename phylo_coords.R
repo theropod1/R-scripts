fe298a002318b8f8e7d142b6d7d79ae2c8287446
Darius Nau <109037333+theropod1@users.noreply.github.com>
2026-06-16 19:17:42 +0200

    Add files via upload

--- phylo_coords.R
+++ phylo_coords.R
@@ -6,6 +6,7 @@
 #' @param plot logical indicating whether to plot phylogeny. If TRUE, function will first attempt to add plot to the active plotting device, unless add==FALSE
 #' @param type Plotting method (can be "cladogram" or "phylogram", see ape::plot.phylo for details
 #' @param add logical indicating whether to try adding tree to current plotting device
+#' @param length length parameter for arrow tips (passed to arrows())
 #' @param yscale optional numeric vector of length two giving as its first element the scale and as its second element the offset to add to the branching coordinate (y in horizontal plot). Default c(1,1) scales values so that terminal branches are centered on integers starting at 1.
 #' @param root.edge add root edge? Can be logical or numeric giving the length of the root edge to plot. If TRUE, attempts to use tree$root.edge to set root edge length
 #' @param horiz logical; plot horizontally (default TRUE)
@@ -13,7 +14,7 @@
 #' @importFrom strap DatePhylo
 #' @export phylo_coords
 
-phylo_coords <- function(tree,ages=NULL,add.terminal=TRUE, plot=FALSE, type="cladogram",add=TRUE,yscale=c(1,1),root.edge=FALSE, horiz=TRUE, ...) {
+phylo_coords <- function(tree,ages=NULL,add.terminal=TRUE, plot=FALSE, type="cladogram",add=TRUE,yscale=c(1,1),root.edge=FALSE, horiz=TRUE,length=0, ...) {
 
 if(root.edge==TRUE & "root.edge"%in% names(tree)) root.edge<-tree$root.edge #try to pull root.edge from tree
 if(root.edge==TRUE & !"root.edge"%in% names(tree)) root.edge<-0 #fallback if no number provided
@@ -123,30 +124,30 @@ if(dev.cur()<2 & horiz) plot(0,0,type="n",xlim=rev(range(x_)),ylim=range(y_),xla
 if(dev.cur()<2 & !horiz) plot(0,0,type="n",xlim=range(x_),ylim=rev(range(y_)),xlab="",ylab="",axes=FALSE)
 if(type!="cladogram"){
 if(horiz){
-arrows(x0=x_[tree$edge[,1]], y0=y_[tree$edge[,1]], x1=x_[tree$edge[,1]], y1=y_[tree$edge[,2]],...)
-arrows(x0=x_[tree$edge[,1]], y0=y_[tree$edge[,2]], x1=x_[tree$edge[,2]], y1=y_[tree$edge[,2]],...)}else{
+arrows(x0=x_[tree$edge[,1]], y0=y_[tree$edge[,1]], x1=x_[tree$edge[,1]], y1=y_[tree$edge[,2]],length=length,...)
+arrows(x0=x_[tree$edge[,1]], y0=y_[tree$edge[,2]], x1=x_[tree$edge[,2]], y1=y_[tree$edge[,2]],length=length,...)}else{
 
-arrows(x0=x_[tree$edge[,1]], y0=y_[tree$edge[,1]], x1=x_[tree$edge[,2]], y1=y_[tree$edge[,1]],...)
-arrows(x0=x_[tree$edge[,2]], y0=y_[tree$edge[,1]], x1=x_[tree$edge[,2]], y1=y_[tree$edge[,2]],...)
+arrows(x0=x_[tree$edge[,1]], y0=y_[tree$edge[,1]], x1=x_[tree$edge[,2]], y1=y_[tree$edge[,1]],length=length,...)
+arrows(x0=x_[tree$edge[,2]], y0=y_[tree$edge[,1]], x1=x_[tree$edge[,2]], y1=y_[tree$edge[,2]],length=length,...)
 }
 
-}else if(type=="cladogram") arrows(x_[tree$edge[,1]], y_[tree$edge[,1]], x_[tree$edge[,2]], y_[tree$edge[,2]],...)
+}else if(type=="cladogram") arrows(x_[tree$edge[,1]], y_[tree$edge[,1]], x_[tree$edge[,2]], y_[tree$edge[,2]],length=length,...)
 
-if(root.edge!=FALSE & is.numeric(root.edge)) if(horiz) arrows(x_[root]+root.edge, y_[root], x_[root], y_[root],...) else arrows(x_[root], y_[root]+root.edge, x_[root], y_[root],...)
+if(root.edge!=FALSE & is.numeric(root.edge)) if(horiz) arrows(x_[root]+root.edge, y_[root], x_[root], y_[root],length=length,...) else arrows(x_[root], y_[root]+root.edge, x_[root], y_[root],...)
 
 }else{#if plotting in "ape space" (tree length)
 
 if(type!="cladogram"){ 
 if(horiz){
-arrows(x[tree$edge[,1]], y[tree$edge[,1]], x[tree$edge[,1]], y[tree$edge[,2]],...)
-arrows(x[tree$edge[,1]], y[tree$edge[,2]], x[tree$edge[,2]], y[tree$edge[,2]],...)}else{
+arrows(x[tree$edge[,1]], y[tree$edge[,1]], x[tree$edge[,1]], y[tree$edge[,2]],length=length,...)
+arrows(x[tree$edge[,1]], y[tree$edge[,2]], x[tree$edge[,2]], y[tree$edge[,2]],length=length,...)}else{
 
-arrows(x[tree$edge[,1]], y[tree$edge[,1]], x[tree$edge[,2]], y[tree$edge[,1]], ...)
-arrows(x[tree$edge[,2]], y[tree$edge[,1]],x[tree$edge[,2]], y[tree$edge[,2]], ...)}
+arrows(x[tree$edge[,1]], y[tree$edge[,1]], x[tree$edge[,2]], y[tree$edge[,1]],length=length, ...)
+arrows(x[tree$edge[,2]], y[tree$edge[,1]],x[tree$edge[,2]], y[tree$edge[,2]],length=length, ...)}
 
-}else if(type=="cladogram") arrows(x0=x[tree$edge[,1]], y0=y[tree$edge[,1]], x1=x[tree$edge[,2]], y1=y[tree$edge[,2]],...)
+}else if(type=="cladogram") arrows(x0=x[tree$edge[,1]], y0=y[tree$edge[,1]], x1=x[tree$edge[,2]], y1=y[tree$edge[,2]],length=length,...)
 
-if(root.edge!=FALSE & is.numeric(root.edge)) if(horiz) arrows(x[root]-root.edge, y[root], x[root], y[root],...) else arrows(x[root], y[root]-root.edge, x[root], y[root],...)
+if(root.edge!=FALSE & is.numeric(root.edge)) if(horiz) arrows(x[root]-root.edge, y[root], x[root], y[root],length=length,...) else arrows(x[root], y[root]-root.edge, x[root], y[root],length=length,...)
 
 }
 
